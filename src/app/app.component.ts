f43bc1cf35ce186225a97291bb093e1bba0f1afc
bhanupratap111 <bhanupratap333.bp@gmail.com>
2020-07-16 11:16:01 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cascrew-frontend';
+  title = 'welcome to cascrew-frontend';
 }
